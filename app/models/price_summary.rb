0c694286251008f0f20d5e078b107d6fad5177f6
kolen <incredible.angst@gmail.com>
2015-10-09 17:16:15 +0300

    Small simplification

--- app/models/price_summary.rb
+++ app/models/price_summary.rb
@@ -32,7 +32,7 @@ class PriceSummary
 
   # Returns price of sum of all items' prices (before discounts)
   def sum_price
-    @sum_price ||= @cart_items.map { |ci| ci.price }.inject(:+)
+    @sum_price ||= @cart_items.map(&:price).inject(:+)
   end
 
   private
